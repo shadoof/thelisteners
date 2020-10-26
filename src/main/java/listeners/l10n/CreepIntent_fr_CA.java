eae08e951768c38c3d36892c6de5b9c9f4c25504
John Cayley <John_Cayley@brown.edu>
2020-10-26 17:26:31 -0400

    Create CreepIntent_fr_CA.java

---
+++ src/main/java/listeners/l10n/CreepIntent_fr_CA.java
@@ -0,0 +1,29 @@
+package listeners.l10n;
+
+import static listeners.util.Utils.S;
+import static listeners.util.Utils.breath;
+import static listeners.util.Utils.breathShort;
+import static listeners.util.Utils.r;
+import static listeners.util.Utils.s;
+
+public class CreepIntent_fr_FR extends CreepIntent {
+
+	public String buildCardTitle() {
+
+		return S("Croyez-vous que nous vous rendons inconfortable?", "Pensez-vous que nous vous rendons inconfortable?");
+	}
+
+	public String buildSpeech() {
+
+		String speech = s("C'est un peu " + r("choquant `perturbant `troublant "),
+				"Nous sommes " + s("choqués", s("un peu") + "vexés")) + breath();
+		speech += "d'apprendre cela. " + breathShort();
+		speech += "Nous n'avons certainement pas " + s("l'intention", "la volonté") + "de "
+				+ s("vous occasioner un malaise.", "‘rendre inconfortable’.") + breathShort();
+		speech += "Nous voulons " + s("seulement", "simplement") + "vous écouter. ";
+		speech += s(breathShort() + "Vous écouter.");
+		speech += breath();
+		return speech;
+	}
+
+}
