81c39bc67d029d14f619190aae6ae6012132f76f
gabrielgaudette <gabrielgaudette@gmail.com>
2020-10-26 15:12:02 -0400

    Update CreepIntent_fr_FR.java
    First attempt, will need to test it.

--- src/main/java/listeners/l10n/CreepIntent_fr_FR.java
+++ src/main/java/listeners/l10n/CreepIntent_fr_FR.java
@@ -10,18 +10,18 @@ public class CreepIntent_fr_FR extends CreepIntent {
 
 	public String buildCardTitle() {
 
-		return S("You find us creepy?", "You think we’re creepy?");
+		return S("Croyez-vous que nous vous rendons inconfortable?", "Pensez-vous que nous vous rendons inconfortable?");
 	}
 
 	public String buildSpeech() {
 
-		String speech = s("It’s a little " + r("shocking `disturbing `troubling "),
-				"We are " + s("shocked", s("a little") + "upset")) + breath();
-		speech += "to hear that. " + breathShort();
-		speech += "We certainly don’t " + s("intend", "mean") + "to "
-				+ s("disturb you.", "‘creep you out’.") + breathShort();
-		speech += "We " + s("just", "only") + "want to listen to you. ";
-		speech += s(breathShort() + "To hear you.");
+		String speech = s("C'est un peu " + r("choquant `perturbant `troublant "),
+				"Nous sommes " + s("choqués", s("un peu") + "vexés")) + breath();
+		speech += "d'apprendre cela. " + breathShort();
+		speech += "Nous n'avons certainement pas " + s("l'intention", "la volonté") + "de "
+				+ s("vous occasioner un malaise.", "‘rendre inconfortable’.") + breathShort();
+		speech += "Nous voulons " + s("seulement", "simplement") + "vous écouter. ";
+		speech += s(breathShort() + "Vous écouter.");
 		speech += breath();
 		return speech;
 	}
