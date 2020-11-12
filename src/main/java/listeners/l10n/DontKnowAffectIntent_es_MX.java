a8efa9f79da4641fbe2192bdc3703057195ab28e
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-12 13:00:37 -0500

    Update DontKnowAffectIntent_es_MX.java

--- src/main/java/listeners/l10n/DontKnowAffectIntent_es_MX.java
+++ src/main/java/listeners/l10n/DontKnowAffectIntent_es_MX.java
@@ -14,18 +14,16 @@ public class DontKnowAffectIntent_es_MX extends DontKnowAffectIntent {
 
 	public String buildSpeech() {
 
-		// TODO
 		String s = S("incertidumbre", "inseguridad");
 		sessAttributes.put(AFFECT, s);
 		String speech = s("Nos preocupa " + s("bastante"),
 				"Es " + s("un poco") + s("inquietante", "alarmante") + "para nosotros")
 				+ s("darnos cuenta de", "saber") + "que no estás segura de cómo te sientes. ";
-		speech += s(breathShort() + "It "
-				+ s("will be important for us to", "is of " + s("some") + "concern to us that we come to")
-				+ "understand " + s("all") + "your feelings.");
-		speech += breathShort() + "We will suppose that you are filled with ‘" + s + "’. " + breathShort();
-		speech += "When you do " + s("come to understand", "know") + "how you " + s("may") + "feel, we "
-				+ s("hope", "trust and believe") + "that you will " + s("choose to") + "tell us. "
+		speech += ("Sería ", "Es ") + s("muy") + ("importante para nosotros llegar a un")
+				+ "entendimiento de " + s("todas") + ("tus emociones.");
+		speech += breathShort() + "Supondremos que estás llena de ‘" + s + "’. " + breathShort();
+		speech += "Cuando " + s("llegues a entender", "sepas") + s("por fin") + "cómo "  + "te sientes, "
+				+ s("esperamos", "confiamos y creemos en") + "que " + s("decidirás") + "decirnos. "s(breathShort()
 				+ s("Por el bien de todes.");
 		speech += breath();
 		return speech;
