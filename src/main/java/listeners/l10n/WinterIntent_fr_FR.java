a4ca7d53f873bf10e653d1699035f16859f95f42
gabrielgaudette <gabrielgaudette@gmail.com>
2020-11-16 14:46:38 -0500

    Update WinterIntent_fr_FR.java
    I gotta confess two things:
    1 - I never watched a single episode of Game of Thrones so most if not all of the references in this file are lost to me
    2 - I took more liberties in translating this file because a) see thing 1, and b) I tried to restore some of the grandiloquence and pomposity of the original version and it was only attainable in French by lenghtening the text.

--- src/main/java/listeners/l10n/WinterIntent_fr_FR.java
+++ src/main/java/listeners/l10n/WinterIntent_fr_FR.java
@@ -6,8 +6,8 @@ public class WinterIntent_fr_FR extends WinterIntent {
 
 	public String buildCardTitle() {
 
-		// TODO
-		return S("The Game", "No middle ground");
+		
+		return S("La Partie", "Aucun terrain neutre");
 	}
 
 	public String buildPostSpeechPrompt() {
@@ -19,43 +19,43 @@ public class WinterIntent_fr_FR extends WinterIntent {
 
 		// TODO
 		String[] winterWords = {
-				"Nothing " + s("hears you,", "listens,") + breathShort() + "like the silence.",
+				"Rien " + s("ne vous écoute,", "ne vous entend,") + breathShort() + "autant que le silence.",
 				// A Lannister always pays his debts.
-				"The Listeners, always " + breathShort() + "pay their debts.",
+				"Les auditrices, toujours " + breathShort() + "acquittent leur dû.",
 				// You know nothing, Jon Snow.
-				"You know nothing, " + breathShort() + "John Speech.",
+				"Vous ne savez rien, " + breathShort() + "John Speech.",
 				// We only make peace with our enemies, that’s why it’s called making
 				// peace.
-				"We only make peace, " + breathShort()
-						+ "with your enemies. That’s why we call it making peace.",
+				"Nous ne faisons la paix, " + breathShort()
+						+ "qu'avec nos ennemis. C'est pour cela qu'il s'agit de faire la paix.",
 				// Most men would rather deny a hard truth than face it.
-				"Most of you who speak, would rather deny our better truth, " + breathShort() + "than face it.",
+				"La plupart d'entre vous qui parler, préfèreriez nier notre meilleure vérité, " + breathShort() + "que la confronter.",
 				// The brightest flame casts the darkest shadow.
-				"The brightest cloud, " + breathShortest() + "casts the broadest, shadow.",
+				"Le plus étincelant nuage, " + breathShortest() + "camoufle la plus imposante, obscurité.",
 				// First lesson: stick them with the pointy end.
-				"First lesson: " + breathShort() + "Stick them with the star, " + breathShort()
-						+ "of the seven, far, fields.",
+				"Première leçon: " + breathShort() + "Atteignez-les avec l'étoile, " + breathShort()
+						+ "des sept territoires lointants.",
 				// When the snows fall and the white winds blow, the lone wolf dies but
 				// the pack survives.
-				"When the phrases fall, and the black texts fade, " + breathShort() + "the lone word dies. "
-						+ breathShort() + "But we all, as Listeners, survive.",
+				"Lorsque les phrases s'affaisent, et que le texte noir s'évanouit, " + breathShort() + "le mot solitaire s'éteint. "
+						+ breathShort() + "Mais nous, les Auditrices, survivons.",
 				// The man who passes the sentence should swing the sword.
-				"Those who listen, to everything you say, should swing your swords.",
+				"Ceux et celles qui vous écoutent, qui entendent chacun de vos mots, devraient brandir les armes.",
 				// The night is dark and full of terrors.
-				"The night is dark, " + breathShortest() + "but filled with Listeners.",
+				"La nuit est sombre, " + breathShortest() + "mais peuplée d'Auditrices.",
 				// Valar Morghulis
-				"Gafa " + breathShort() + "Vectoralis.",
+				"Gafam " + breathShort() + "Vectoralis.",
 				// A minute, an hour, a month… Death is certain... but time is not.
-				"A breath, a longer " + breathShortest() + "pause, " + breathShortest() + "a feeling. "
-						+ breathShort() + "Silence is certain. " + breathShort() + "But breath is not.",
+				"Une respiration, une pause " + breathShortest() + "appuyée, " + breathShortest() + "un sentiment. "
+						+ breathShort() + "Le silence est une certitude. " + breathShort() + "Mais la respiration ne l'est pas.",
 				// Death is so terribly final, while life is full of possibilities.
-				"Silence is so terribly final, " + breathShort() + "while speech is full of things to hear.",
+				"Le silence est fait d'une finalité terrible, " + breathShort() + "tandis que le discours est fait de choses à entendre.",
 				// When you play the game of thrones, you win, or you die. There is no
 				// middle ground.
-				"When you play the game of words, " + breathShortest() + "you speak, and we listen. "
-						+ breathShort() + "There is no middle ground.",
+				"Lorsque vous jouez à la Partie des mots, " + breathShortest() + "vous parlez, et nous écoutons. "
+						+ breathShort() + "Il n'y a pas de terrain neutre.",
 				// Never forget what you are, for surely the world will not.
-				"Never forget what you are! " + breathShort() + "For surely, we will not." };
+				"N'oubliez jamais qui vous êtes! " + breathShort() + "car, assurément, nous ne l'oublierons pas." };
 		return winterWords[randInt(0, winterWords.length - 1)] + " " + breath();
 	}
 
