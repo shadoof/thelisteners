2b96dcac71539265ae2befd245b8273843d566ee
gabrielgaudette <gabrielgaudette@gmail.com>
2020-11-16 14:31:00 -0500

    Update WhoseAccountIntent_fr_FR.java
    Line 19 : not sure what impact it will have but the "PERFORMANCE ?" command was moved to accommodate the translation to French.

--- src/main/java/listeners/l10n/WhoseAccountIntent_fr_FR.java
+++ src/main/java/listeners/l10n/WhoseAccountIntent_fr_FR.java
@@ -9,19 +9,19 @@ public class WhoseAccountIntent_fr_FR extends WhoseAccountIntent {
 	public String buildCardTitle() {
 
 		// TODO
-		return S("Our profile", "This " + s("little") + "account of ours");
+		return S("Notre profil", "Ce " + s("modeste") + "compte qui est le nôtre");
 	}
 
 	public String buildSpeech() {
 
-		// TODO
-		String speech = "These conversations, " + s("and these transactions,")
-				+ "take place within the scope of, " + (PERFORMANCE ? "‘John’s’ " : "your ") + "profile. "
+	
+		String speech = "Ces conversations, " + s("et ces transactions,")
+				+ "se déroulent " + s("sous l'égide du,", "au sein du") + "profil " + s(PERFORMANCE ? "de ‘John’. " : "qui est le vôtre."
 				+ breath();
 		speech += PERFORMANCE
-				? s("If we are " + s(s("ever") + "part of an installation,", "gallery installed,")
-						+ "then everything is sent, " + s("not only to the cloud, but") + "to our "
-						+ s("app.", "app, as configured by our artist."))
+				? s("Si" + s("d'aventure") + "nous faisons partie"  + s("d'une exposition,", "d'une installation,", "d'une performance dans une galerie artistique")
+						+ "dans ce cas, tout transite, " + s("non seulement dans le nuage, mais également") + "à travers notre "
+						+ s("application.", "application, telle que configurée par notre artiste."))
 				: "";
 		return speech + breath();
 	}
