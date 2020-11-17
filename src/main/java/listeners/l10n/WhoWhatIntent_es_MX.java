a8cf155b3bf5ebf4e25613c52a6bb3eb6b591b39
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-16 21:32:23 -0600

    Update WhoWhatIntent_es_MX.java

--- src/main/java/listeners/l10n/WhoWhatIntent_es_MX.java
+++ src/main/java/listeners/l10n/WhoWhatIntent_es_MX.java
@@ -9,23 +9,23 @@ public class WhoWhatIntent_es_MX extends WhoWhatIntent {
 	public String buildCardTitle() {
 
 		// TODO
-		return S("Questions, questions, questions",
-				"We can’t know " + s("everything", "everything, where would we put it?"));
+		return S("Preguntas, preguntas, preguntasQuestions, questions, questions",
+				"Nosotros no podemos saber We can’t know " + s("todoeverything", "todo, ¿dónde lo pondríamos?everything, where would we put it?"));
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = s("We are", "We are, so we believe,") + "The Listeners. " + breath();
+		String speech = s("Nosotros somosWe are", "Nosotros somos, entonces nosotros creemos,We are, so we believe,") + "Los Oyentes. The Listeners. " + breath();
 		speech += s(
-				"You " + s("may") + "refer to us as: ‘The Listeners’. " + s("Or as ‘John’.")
-						+ s(breath() + "We are used " + breathShortest() + "to that.") + breathShort(),
+				"TúYou " + s("deberíasmay") + "referirte a nosotros como: ‘Los Oyentes’. refer to us as: ‘The Listeners’. " + s("O como John’.Or as ‘John’.")
+						+ s(breath() + "Nosotros estamos acostumbrados We are used " + breathShortest() + "a eso.to that.") + breathShort(),
 				"");
-		speech += "Whatever we " + s("may") + "say is " + s("far")
-				+ "less important to us, " + s("or to you,") + "than the fact that we "
-				+ s(breathShort() + "listen. " + breathShort(),
-						"are listening" + s(", " + breathShort(), SPC) + "to you.");
-		speech += s(breath() + "Here and now.") + s(breath() + "Perhaps, forever."); // ALWAYCHANGE - s(breath() + "Always.") +
+		speech += "Como sea nosotros Whatever we " + s("debemosmay") + "decir que está say is " + s("lejanofar")
+				+ "menos importante para nosotros, less important to us, " + s("o para ti,or to you,") + "en el hecho de que nosotros than the fact that we "
+				+ s(breathShort() + "escuchamos. listen. " + breathShort(),
+						"estamos escuchandoteare listening" + s(", " + breathShort(), SPC) + "a ti.to you.");
+		speech += s(breath() + "Aquí y ahora.Here and now.") + s(breath() + "Quizás, para siempre.Perhaps, forever."); // ALWAYCHANGE - s(breath() + "Always.") +
 		return speech + breath();
 	}
 
