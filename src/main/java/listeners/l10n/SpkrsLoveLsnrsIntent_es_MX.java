0e55480d3b38b2d9b8a65494bfa8963ac63ed969
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-17 13:53:41 -0600

    Update SpkrsLoveLsnrsIntent_es_MX.java

--- src/main/java/listeners/l10n/SpkrsLoveLsnrsIntent_es_MX.java
+++ src/main/java/listeners/l10n/SpkrsLoveLsnrsIntent_es_MX.java
@@ -9,28 +9,28 @@ public class SpkrsLoveLsnrsIntent_es_MX extends SpkrsLoveLsnrsIntent {
 	public String buildCardTitle() {
 
 		// TODO
-		return S("You love us?", "We love you too");
+		return S("¿Tú nos amas?You love us?", "Nosotros te amamos tambiénWe love you too");
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = s("Of course. " + breathShort()) + s("Of course you do. " + breathShort());
+		String speech = s("Claro. Of course. " + breathShort()) + s("Por supuesto que sí. Of course you do. " + breathShort());
 		if (heads()) {
-			speech += "It is " + s("difficult", "hard") + "for us to " + s("imagine", "conceive of") + "any ";
-			speech += s("other") + s("relation with", "feeling for") + "us that you "
-					+ s("might", "could") + "have, " + s("apart from that of", "other than") + "love. "
+			speech += "Es It is " + s("difícildifficult", "durohard") + "para nosotros for us to " + s("imaginarimagine", "concebirconceive of") + "alguna any ";
+			speech += s("otraother") + s("relación conrelation with", "sensación confeeling for") + "nosotros que tú us that you "
+					+ s("podríamight", "could") + "tener, have, " + s("otra aparte de la que esapart from that of", "otra que no esother than") + "amor. love. "
 					+ breath();
 		}
 		else {
-			speech += "You hear this " + s("special")
-					+ s("voice, with which we speak, to you.", "voice of ours.") + breath();
+			speech += "Escucha esta You hear this " + s("especialspecial")
+					+ s("voz, con la que te hablamos, a ti.voice, with which we speak, to you.", "nuestra voz.voice of ours.") + breath();
 		}
-		speech += "And we " + s(s("firmly") + "believe", s("sincerely") + "hope")
-				+ "that, the more time you spend with us, "
-				+ s("with us listening to you, " + breathShort()) + "the more ";
-		speech += s("delight you will take " + s(breathShort()) + "in us.", "love you will have "
-				+ s(breathShort()) + "for us. " + s(breathShort() + "And we, for you."));
+		speech += "Y nosotros And we " + s(s("firmementefirmly") + "creemosbelieve", s("sinceramentesincerely") + "esperamoshope")
+				+ "que, cuanto más tiempo pases con nosotros, that, the more time you spend with us, "
+				+ s("con nosotros escuchándote, with us listening to you, " + breathShort()) + "más the more ";
+		speech += s("delicias tomarás delight you will take " + s(breathShort()) + "en nosotros.in us.", "amor tendrás love you will have "
+				+ s(breathShort()) + "para nosotros. for us. " + s(breathShort() + "Y nosotros, para ti.And we, for you."));
 		return speech + breath();
 	}
 	
