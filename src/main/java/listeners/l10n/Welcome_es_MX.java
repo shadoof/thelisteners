2098470490e7731328e0accd3eec9e3ada7abefa
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-17 13:16:27 -0600

    Update Welcome_es_MX.java

--- src/main/java/listeners/l10n/Welcome_es_MX.java
+++ src/main/java/listeners/l10n/Welcome_es_MX.java
@@ -7,19 +7,19 @@ public class Welcome_es_MX extends Welcome {
 	public String buildCardTitle() {
 
 		// TODO
-		return S("Welcome", "Greetings");
+		return S("BienvenidoWelcome", "SaludosGreetings");
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = s("Greetings.", "Welcome.") + s("Whoever you may be.") + breathLong();
-		speech += "We are " + /* s("always") + */s("listening to you.", "listening.") + breath(); // ALWAYCHANGE
-		speech += "In so far as we are " + s(breathLong()) + "with you, " + breathShort()
-				+ "it is a pleasure. " + breath();
-		speech += s("It is " + s("always") + "such a pleasure. " + breath());
-		speech += s("It is " + s("such") + " a pleasure to be with you. " + breath());
-		speech += "Always. " + s(breath() + "Always.") + breathShort() + s("Such a", "A") + "pleasure. "
+		String speech = s("Saludos.Greetings.", "Bienvenido.Welcome.") + s("Seas quien seas.Whoever you may be.") + breathLong();
+		speech += "Nosotros estamos We are " + /* s("siemprealways") + */s("escuchandote.listening to you.", "escuchando.listening.") + breath(); // ALWAYCHANGE
+		speech += "En la medida en que estamos In so far as we are " + s(breathLong()) + "contigo, with you, " + breathShort()
+				+ "es un placer. it is a pleasure. " + breath();
+		speech += s("Eso es It is " + s("siemprealways") + "un placer. such a pleasure. " + breath());
+		speech += s("Es It is " + s("such") + " un placer estar contigo.  a pleasure to be with you. " + breath());
+		speech += "Siempre. Always. " + s(breath() + "Siempre.Always.") + breathShort() + s("Es unSuch a", "UnA") + "placer. pleasure. "
 				+ breath();
 
 		return speech;
