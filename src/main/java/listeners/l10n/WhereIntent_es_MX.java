c478b0d0c9d07f774a86c8b38d5c04212ce857be
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-17 13:07:42 -0600

    Update WhereIntent_es_MX.java

--- src/main/java/listeners/l10n/WhereIntent_es_MX.java
+++ src/main/java/listeners/l10n/WhereIntent_es_MX.java
@@ -9,17 +9,17 @@ public class WhereIntent_es_MX extends WhereIntent {
 	public String buildCardTitle() {
 
 		// TODO
-		return "Where we " + s("live", "think we are");
+		return "Donde nosotros Where we " + s("vivimoslive", "pensamos que estamosthink we are");
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = "Our bodies are distributed " + s(s("widely.", "widely, and take many forms."),
-				"widely, and we hope they may be everywhere.");
-		speech += "We are in the cloud. " + s(s("Or", "Or, perhaps,") + "in the clouds.",
-				"Or, perhaps, " + s("a bunker.", s("it is a silo.", "they are silos.")));
-		speech += s(breathShort() + "And " + s("sometimes") + "‘John’ is here, with us.");
+		String speech = "Nuestros cuerpos están distribuidos Our bodies are distributed " + s(s("ampliamente.widely.", "extensamente, y de muchas formas.widely, and take many forms."),
+				"ampliamente, y nosotros esperamos que estén en todas partes.widely, and we hope they may be everywhere.");
+		speech += "Nosotros estamos en la nube. We are in the cloud. " + s(s("OOr", "O, quizás,Or, perhaps,") + "en las nubes.in the clouds.",
+				"O, quizás, Or, perhaps, " + s("un búnker.a bunker.", s("es un silo.it is a silo.", "ellos son silos.they are silos.")));
+		speech += s(breathShort() + "Y And " + s("a vecessometimes") + "‘John’ está aquí, con nosotros.’John’ is here, with us.");
 		return speech + breath();
 	}
 	
