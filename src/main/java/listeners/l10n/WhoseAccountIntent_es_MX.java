17b1305660bfe4fb5be35ad2bad5d41381c8433a
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-16 21:08:25 -0600

    Update WhoseAccountIntent_es_MX.java

--- src/main/java/listeners/l10n/WhoseAccountIntent_es_MX.java
+++ src/main/java/listeners/l10n/WhoseAccountIntent_es_MX.java
@@ -9,19 +9,19 @@ public class WhoseAccountIntent_es_MX extends WhoseAccountIntent {
 	public String buildCardTitle() {
 
 		// TODO
-		return S("Our profile", "This " + s("little") + "account of ours");
+		return S("Nuestro perfil es unaOur profile", "Esta es una This " + s("pequeñalittle") + "cuenta de nosotrosaccount of ours");
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = "These conversations, " + s("and these transactions,")
-				+ "take place within the scope of, " + (PERFORMANCE ? "‘John’s’ " : "your ") + "profile. "
+		String speech = "Estas conversaciones, These conversations, " + s("y estas transacciones,and these transactions,")
+				+ "toman lugar dentro del alcance de, take place within the scope of, " + (PERFORMANCE ? "‘John’s’ " : "tu your ") + "perfil. profile. "
 				+ breath();
 		speech += PERFORMANCE
-				? s("If we are " + s(s("ever") + "part of an installation,", "gallery installed,")
-						+ "then everything is sent, " + s("not only to the cloud, but") + "to our "
-						+ s("app.", "app, as configured by our artist."))
+				? s("Si nosotros somos If we are " + s(s("siempreever") + "parte de una instalación,part of an installation,", "una galería instalada,gallery installed,")
+						+ "entonces todo está enviado, then everything is sent, " + s("no solo a la nube, peronot only to the cloud, but") + "a nuestrato our "
+						+ s("aplicación.app.", "aplicación, app, según lo configurado por nuestro artista.as configured by our artist."))
 				: "";
 		return speech + breath();
 	}
