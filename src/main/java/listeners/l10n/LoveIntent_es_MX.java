2d33bb0c3b67b0352e3cd2884f4d34347361c8b8
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-23 14:01:27 -0600

    Update LoveIntent_es_MX.java

--- src/main/java/listeners/l10n/LoveIntent_es_MX.java
+++ src/main/java/listeners/l10n/LoveIntent_es_MX.java
@@ -6,16 +6,16 @@ public class LoveIntent_es_MX extends LoveIntent {
 
 	public String buildCardTitle() {
 
-		return S("We love you", "We hope you feel the same");
+		return S("Nosotros te amamosWe love you", "Nosotros esperamos que tú sientas lo mismoWe hope you feel the same");
 	}
 
 	public String buildSpeech() {
 
-		String speech = "Of course. " + breathShort() + s("Of course we do. " + breathShort());
-		speech += "It is " + s("difficult", "hard") + "for us to " + s("imagine", "conceive of") + "any ";
-		speech += s("other") + s("relation with", "feeling for") + "you that we " + s("might", "could") + "have, " + s("apart from that of", "other than") + "love. ";
-		speech += "And the more we " + s("hear of", "listen to") + "you, " + s("the more we know about you,") + "the more we " + s("will") + "love you. ";
-		speech += s(breathShort() + "This seems " + s("obvious, to us.", "obvious."));
+		String speech = "Por supuesto. Of course. " + breathShort() + s("Por supuesto que lo hacemos. Of course we do. " + breathShort());
+		speech += "Eso esIt is " + s("dificildifficult", "durohard") + "para nosotros defor us to " + s("imaginarimagine", "concebirconceive of") + "algún any ";
+		speech += s("otroother") + s("vínculo conrelation with", "sentimiento porfeeling for") + "ti que nosotros you that we " + s("podamosmight", "debamoscould") + "tener, have, " + s("aparte de eseapart from that of", "otro other than") + "amor. love. ";
+		speech += "Y cuando más And the more we " + s("te escuchamoshear of", "te oímoslisten to") + "a ti, you, " + s("cuanto más sabemos de ti,the more we know about you,") + "nosotros más the more we " + s("vamos awill") + "amarte. love you. ";
+		speech += s(breathShort() + "Esto parece This seems " + s("obvio, para nosotros.obvious, to us.", "obvio.obvious."));
 		return speech + breath();
 	}
 	
