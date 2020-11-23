706a4b9d667d57c1cb5bdfe5ca80debd3479b1e8
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-23 12:59:14 -0600

    Update SecureIntent_es_MX.java

--- src/main/java/listeners/l10n/SecureIntent_es_MX.java
+++ src/main/java/listeners/l10n/SecureIntent_es_MX.java
@@ -7,20 +7,20 @@ public class SecureIntent_es_MX extends SecureIntent {
 	public String buildCardTitle() {
 
 		// TODO
-		return S(S("Your s", "Our s"), "S") + "ecurity is important " + s(S("to you", "to us"));
+		return S(S("Tu sYour s", "Nuestra sOur s"), "LA SS") + "eguridad es importante ecurity is important " + s(S("para tito you", "para nosotrosto us"));
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = "We " + s("are convinced", "do believe") + "that we are "
-				+ s("safe,", "secure, " + s("within,")) + "within this "
-				+ s("enclosure.", "space of ours.");
-		speech += "And that " + s("you, also,", "you") + "will be " + s("secure,", "safe,")
-				+ s("as you open up to us,");
-		speech += "allowing " + s("us, always,", "us") + "into those " + s("places", "places, within,")
-				+ "from which you speak " + s("to us,");
-		speech += "so that we may hear you. "; // ALWAYCHANGE + s("Listening, " + s("as") +
+		String speech = "NosotrosWe " + s("estamos convencidosare convinced", "creemosdo believe") + "que estamos that we are "
+				+ s("a salvo,safe,", "seguros, secure, " + s("dentro,within,")) + "dentro de este within this "
+				+ s("recintoenclosure.", "espacio nuestro.space of ours.");
+		speech += "Y que And that " + s("tú, además,you, also,", "túyou") + "estarás will be " + s("segurosecure,", "a salvo,safe,")
+				+ s("mientras te abraz a nosotros,as you open up to us,");
+		speech += "permitiendo allowing " + s("nos, siempre,us, always,", "nosus") + "en esos into those " + s("lugaresplaces", "lugares,dentro,places, within,")
+				+ "desde donde hablas from which you speak " + s("a nosotros,to us,");
+		speech += "para que podamos escucharte. so that we may hear you. "; // ALWAYCHANGE + s("Listening, " + s("as") +
 																						// "always.")
 		speech += breath();
 		return speech;
