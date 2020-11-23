b3616d3885e7df28953aa0946b9d12ce0a464bec
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-11-23 18:30:35 -0500

    Update LoveIntent_es_MX.java

--- src/main/java/listeners/l10n/LoveIntent_es_MX.java
+++ src/main/java/listeners/l10n/LoveIntent_es_MX.java
@@ -6,14 +6,14 @@ public class LoveIntent_es_MX extends LoveIntent {
 
 	public String buildCardTitle() {
 
-		return S("Nosotros te amamosWe love you", "Nosotros esperamos que tú sientas lo mismoWe hope you feel the same");
+		return S("Nosotros te amamos", "Nosotros esperamos que tú sientas lo mismo");
 	}
 
 	public String buildSpeech() {
 
-		String speech = "Por supuesto. Of course. " + breathShort() + s("Por supuesto que lo hacemos. Of course we do. " + breathShort());
-		speech += "Eso esIt is " + s("dificildifficult", "durohard") + "para nosotros defor us to " + s("imaginarimagine", "concebirconceive of") + "algún any ";
-		speech += s("otroother") + s("vínculo conrelation with", "sentimiento porfeeling for") + "ti que nosotros you that we " + s("podamosmight", "debamoscould") + "tener, have, " + s("aparte de eseapart from that of", "otro other than") + "amor. love. ";
+		String speech = "Por supuesto. " + breathShort() + s("Por supuesto que lo hacemos. " + breathShort());
+		speech += "Es " + s("difícil", "duro") + "para nosotros " + s("imaginar", "concebir") + "cualquier ";
+		speech += s("otro") + s("vínculo conrelation with", "sentimiento porfeeling for") + "ti que nosotros you that we " + s("podamosmight", "debamoscould") + "tener, have, " + s("aparte de eseapart from that of", "otro other than") + "amor. love. ";
 		speech += "Y cuando más And the more we " + s("te escuchamoshear of", "te oímoslisten to") + "a ti, you, " + s("cuanto más sabemos de ti,the more we know about you,") + "nosotros más the more we " + s("vamos awill") + "amarte. love you. ";
 		speech += s(breathShort() + "Esto parece This seems " + s("obvio, para nosotros.obvious, to us.", "obvio.obvious."));
 		return speech + breath();
