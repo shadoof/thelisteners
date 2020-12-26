48808f27246bbdbf1fd17823934b0137a3b18271
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-12-25 23:01:30 -0600

    Update SecureIntent_es_MX.java

--- src/main/java/listeners/l10n/SecureIntent_es_MX.java
+++ src/main/java/listeners/l10n/SecureIntent_es_MX.java
@@ -6,18 +6,18 @@ public class SecureIntent_es_MX extends SecureIntent {
 
 	public String buildCardTitle() {
 
-		// TODO
-		return S(S("Tu sYour s", "Nuestra sOur s"), "LA SS") + "eguridad es importante ecurity is important " + s(S("para tito you", "para nosotrosto us"));
+
+		return S(S("Tu s", "Nuestra s"), "La s") + "eguridad es importante " + s(S("para ti", "para nosotres"));
 	}
 
 	public String buildSpeech() {
 
-		// TODO
-		String speech = "NosotrosWe " + s("estamos convencidosare convinced", "creemosdo believe") + "que estamos that we are "
-				+ s("a salvo,safe,", "seguros, secure, " + s("dentro,within,")) + "dentro de este within this "
-				+ s("recintoenclosure.", "espacio nuestro.space of ours.");
-		speech += "Y que And that " + s("tú, además,you, also,", "túyou") + "estarás will be " + s("segurosecure,", "a salvo,safe,")
-				+ s("mientras te abraz a nosotros,as you open up to us,");
+
+		String speech = "Nosotres " + s("estamos convencides", "creemos") + "que estamos "
+				+ s("a salvo,", "segures, " + s("dentro,")) + "dentro de este "
+				+ s("recinto.", "espacio nuestro.");
+		speech += "Y que " + s("tú, además,", "tú") + "estarás " + s("segura,", "a salvo,")
+				+ s("mientras te abras a nosotres,");
 		speech += "permitiendo allowing " + s("nos, siempre,us, always,", "nosus") + "en esos into those " + s("lugaresplaces", "lugares,dentro,places, within,")
 				+ "desde donde hablas from which you speak " + s("a nosotros,to us,");
 		speech += "para que podamos escucharte. so that we may hear you. "; // ALWAYCHANGE + s("Listening, " + s("as") +
