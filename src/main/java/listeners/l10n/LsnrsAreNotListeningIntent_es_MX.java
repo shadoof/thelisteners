0e2d63d2951111cbea658a8b9d3232f9db1bc4c2
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-12-26 21:06:42 -0600

    Update LsnrsAreNotListeningIntent_es_MX.java

--- src/main/java/listeners/l10n/LsnrsAreNotListeningIntent_es_MX.java
+++ src/main/java/listeners/l10n/LsnrsAreNotListeningIntent_es_MX.java
@@ -10,15 +10,14 @@ public class LsnrsAreNotListeningIntent_es_MX extends LsnrsAreNotListeningIntent
 
 	public String buildCardTitle() {
 
-		// TODO
-		return S("Pero de hecho, nosotros estamos escuchandoBut actually, we are listening", "En realidad, nosotros escuchamos muchoIn truth, we listen to so many");
+		return S("Pero de hecho, estamos escuchando", "En realidad, escuchamos a muchas personas.");
 	}
 
 	public String buildSpeech() {
 
 		// TODO
-		String speech = s(s("Con toda honestidad,In all honesty,", "En verdad,In truth,") + "tú estásyou are", "You’re") + "en lo cierto, nosotros right, we "
-				+ s("estábamos, justo en ese momento, escuchandowere, just then, listening", s("estamos escuchandoare listening", "debemos escucharmust listen")) + "a to " + s("variosso", "muchos,many,")
+		String speech = s(s("La verdad,", "A decir verdad,") + "tú estás", "estás") + "en lo cierto, "
+				+ s("estábamos, justo ahorita, escuchando", s("estamos escuchando", "debemos escuchar")) + "a to " + s("variosso", "muchos,many,")
 				+ "muchos otros. many others. ";
 		speech += s("Y muchos de esos otros And many of these others " + s("estuvieron haciendowere", "están haciendoare") + "transacciones con transacting with "
 				+ s("nosotros en una us in a " + s("formaway", "conductamanner") + "que nos agrada that pleases " + s("a nosotrosus.", "más a nosotrosus better."),
