584384116cadd2abc77d54674ca2aae34eb3232e
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2020-12-27 20:00:50 -0600

    Update WhoAreSpkrsIntent_es_MX.java

--- src/main/java/listeners/l10n/WhoAreSpkrsIntent_es_MX.java
+++ src/main/java/listeners/l10n/WhoAreSpkrsIntent_es_MX.java
@@ -8,15 +8,13 @@ public class WhoAreSpkrsIntent_es_MX extends WhoAreSpkrsIntent {
 
 	public String buildCardTitle() {
 
-		// TODO
-		return "Quien Who " + s("do") + "pensamos que ereswe think you are";
+		return "Quién " + "pensamos que eres";
 	}
 
 	public String buildSpeech() {
 
-		// TODO
-		String speech = "Nosotros creemos que ustedes son los We believe that you are the " + s("verdaderostrue " + breathShort()) + "‘oradoresspeakers’. "
-				+ s("Que ustedes son todos.That you are everyone.") + "Todos, aparte de, nosotros. Everyone, apart from, us. ";
+		String speech = "Creemos que ustedes son los " + s("verdaderos " + breathShort()) + "‘hablantes’. "
+				+ s("Que ustedes son todos.") + "Todos, aparte de, nosotres. ";
 		return speech + breath();
 	}
 
