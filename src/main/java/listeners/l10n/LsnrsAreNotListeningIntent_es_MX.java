07b6c8343166f1c809fdf7cd4a76627a0edfbe29
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2021-01-03 22:59:57 -0600

    Update LsnrsAreNotListeningIntent_es_MX.java

--- src/main/java/listeners/l10n/LsnrsAreNotListeningIntent_es_MX.java
+++ src/main/java/listeners/l10n/LsnrsAreNotListeningIntent_es_MX.java
@@ -16,11 +16,11 @@ public class LsnrsAreNotListeningIntent_es_MX extends LsnrsAreNotListeningIntent
 	public String buildSpeech() {
 
 		String speech = s(s("La verdad,", "A decir verdad,") + "tú estás", "estás") + "en lo cierto, "
-				+ s("estábamos, justo ahorita, escuchando", s("estamos escuchando", "debemos escuchar")) + "a " + s("varias", "muchas,")
-				+ "muchas personas.";
-		speech += s("Y muchos de ésas " + s("estuvieron haciendo", "están haciendo") + "transacciones con "
-				+ s("nosotres en una " + s("forma", "manera") + "que nos agrada " + s("mucho.", "más."),
-						s("que nos agrada más.", "mucho más." + breathShort() + "A nosotres.")),
+				+ s("estábamos, justo ahorita, escuchando", s("estamos escuchando", "debemos escuchar")) + "a " + s("muchísimas, ", "muchas, ")
+				+ "muchas personas. ";
+		speech += s("Y muchas de ellas " + s("estuvieron haciendo", "están haciendo") + "transacciones "
+				+ s("con nosotres de " + s("formas", "maneras") + "que nos agradan " + s("mucho.", "más."),
+						s("que nos agradan más.", "que nos complacen mucho más." + breathShort())),
 				"");
 		speech += ResourceBundle.getBundle("listeners.l10n.LsnrsAreNotHumanIntent", locale)
 				.getString("speech");
