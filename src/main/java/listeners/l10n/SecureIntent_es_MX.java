bd09fcf56afb46ad56397c6be44c99eb6950158f
marianaroaoliva <72371040+marianaroaoliva@users.noreply.github.com>
2021-02-28 22:31:49 -0600

    Update SecureIntent_es_MX.java

--- src/main/java/listeners/l10n/SecureIntent_es_MX.java
+++ src/main/java/listeners/l10n/SecureIntent_es_MX.java
@@ -15,7 +15,7 @@ public class SecureIntent_es_MX extends SecureIntent {
 
 		String speech = "Nosotres " + s("no tenemos duda", "tenemos la certeza") + "de que estamos "
 				+ s("a salvo,", "segures, " + s("aquí adentro,")) + "dentro de este "
-				+ s("recinto.", "espacio nuestro.");
+				+ s("recinto.", "espacio.");
 		speech += "Y de que " + s("tú, además,", "tú") + "estarás " + s("segura,", "a salvo,")
 				+ s("mientras te abras a nosotres,");
 		speech += "permitiéndonos" + s(" siempre,", " a nosotres") + "acceder a esos " + s("lugares internos", "sitios,")
